5c7135f17e0f95742181803cca2cd716ac8462c1
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-03-19 17:04:17 +0100

    Add solution. Task#1

---
+++ src/House.java
@@ -0,0 +1,18 @@
+
+//Создайте абстрактный класс "Дом". В качестве аргумента конструктора принимайте адрес.
+//    Напишите для адреса сеттер и геттер.
+public abstract class House {
+  String address;
+  public House(String address)
+  {
+    this.address=address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+}
